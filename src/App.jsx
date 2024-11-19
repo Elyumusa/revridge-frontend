17d92900de51c6b3cf91f9dc59a1cfb583ff4b55
Elyumusa <elyuedu4@gmail.com>
2024-11-19 03:31:09 +0300

    Updated vite.config.js so all assets can be included

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-
+import React, { lazy, Suspense } from 'react';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider,Route } from 'react-router-dom'
 import RootLayout from './pages/layout'
-import HomePage from './pages/page'
-import SupportPage from './pages/support/page'
-import StockPredictorPage from './pages/stock-predictor/page'
-import CompliancePage from './pages/compliance/page'
-import BlogPage from './pages/blog/page'
-import AboutPage from './pages/about/page'
-import NotFoundPage from './pages/notfound/NotFoundPage'
-import ArticlePage from './pages/blog/article_page.jsx'
-import DividendCalendar from './pages/dividendCalender/DividendCalender'
+// import HomePage from './pages/page'
+// import SupportPage from './pages/support/page'
+// import StockPredictorPage from './pages/stock-predictor/page'
+// import CompliancePage from './pages/compliance/page'
+// import BlogPage from './pages/blog/page'
+// import AboutPage from './pages/about/page'
+// import NotFoundPage from './pages/notfound/NotFoundPage'
+// import ArticlePage from './pages/blog/article_page.jsx'
+// import DividendCalendar from './pages/dividendCalender/DividendCalender'
+
+const HomePage = lazy(() => import('./pages/page'));
+const SupportPage = lazy(() => import('./pages/support/page'));
+const StockPredictorPage = lazy(() => import('./pages/stock-predictor/page'));
+const CompliancePage = lazy(() => import('./pages/compliance/page'));
+const BlogPage = lazy(() => import('./pages/blog/page'));
+const AboutPage = lazy(() => import('./pages/about/page'));
+const NotFoundPage = lazy(() => import('./pages/notfound/NotFoundPage'));
+const ArticlePage = lazy(() => import('./pages/blog/article_page.jsx'));
+const DividendCalendar = lazy(() => import('./pages/dividendCalender/DividendCalender'));
 
 function App() {
 
--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
-  publicDir: 'src/assets',
+  //publicDir: 'src/assets',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
